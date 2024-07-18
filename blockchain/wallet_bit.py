131ad2034f01ec1559edeb32633a2121f527f81d
Carlos Sousa <103946120+kcarww@users.noreply.github.com>
2024-07-18 11:52:06 -0300

    Atualizar o wallet_bit.py

--- blockchain/wallet_bit.py
+++ blockchain/wallet_bit.py
@@ -8,3 +8,16 @@ print(f'My address: {address}')
 
 ballance = key.get_balance()
 print(f'Balance: {ballance}')
+
+
+import requests
+
+# Obtendo informações de um bloco específico
+block_hash = '0000000000000000000b4d0b7bda2b8657f8e982f6e7fdf2f5b71e8b1b4cfc1c'
+url = f'https://blockchain.info/rawblock/{block_hash}'
+response = requests.get(url)
+block_data = response.json()
+
+print(f"Altura do bloco: {block_data['height']}")
+print(f"Tempo: {block_data['time']}")
+print(f"Número de transações: {len(block_data['tx'])}")
